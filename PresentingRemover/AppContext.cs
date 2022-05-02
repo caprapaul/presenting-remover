44d4d77a8450b5c72df8891382b5cb90f41e420d
Paul Capra <c.paulica@gmai.com>
2022-05-02 22:50:18 +0300

    Added project

---
+++ PresentingRemover/AppContext.cs
@@ -0,0 +1,69 @@
+﻿using Windows.Win32;
+using Windows.Win32.Foundation;
+using PresentingRemover.Properties;
+
+namespace PresentingRemover;
+
+public class AppContext : ApplicationContext
+{
+    private readonly NotifyIcon _trayIcon;
+    
+    private HWND _window;
+
+    public AppContext()
+    {
+        _trayIcon = new NotifyIcon
+        {
+            Icon = Resources.Icon,
+            ContextMenuStrip = new ContextMenuStrip
+            {
+                Items =
+                {
+                    new ToolStripMenuItem("Hide", null, Hide),
+                    new ToolStripMenuItem("Exit", null, Exit)
+                }
+            },
+            Visible = true,
+        };
+    }
+
+    private void Hide(object? sender, EventArgs e)
+    {
+        var windowsEnumerable = GetWindows();
+        var windows = windowsEnumerable as List<HWND> ?? windowsEnumerable.ToList();
+        
+        _window = windows
+            .FirstOrDefault(window => User32Extensions.GetWindowText(window).Equals("Screen sharing toolbar", StringComparison.OrdinalIgnoreCase));
+
+        PInvoke.MoveWindow(_window, 0, -1000, 0, 0, true);
+    }
+    
+    public static IEnumerable<HWND> GetWindows()
+    {
+        return User32Extensions.FindWindows((HWND wnd, LPARAM param) =>
+        {
+            if (!PInvoke.IsWindow(wnd))
+            {
+                return false;
+            }
+
+            if (PInvoke.GetWindowTextLength(wnd) == 0)
+            {
+                return false;
+            }
+
+            if (wnd == PInvoke.GetShellWindow())
+            {
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    private void Exit(object? sender, EventArgs e)
+    {
+        _trayIcon.Visible = false;
+        Application.Exit();
+    }
+}
---
+++ PresentingRemover/User32MethodsExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+namespace PresentingRemover;
+
+public static class User32Extensions
+{
+    public static IEnumerable<HWND> FindWindows(WNDENUMPROC filter)
+    {
+        var windows = new List<HWND>();
+
+        PInvoke.EnumWindows((wnd, param) =>
+            {
+                if (filter(wnd, param))
+                {
+                    // only add the windows that pass the filter
+                    windows.Add(wnd);
+                }
+
+                // but return true here so that we iterate all windows
+                return true;
+            },
+            IntPtr.Zero);
+
+        return windows;
+    }
+
+    public static string GetWindowText(HWND handle)
+    {
+        var size = PInvoke.GetWindowTextLength(handle);
+
+        if (size <= 0)
+        {
+            return string.Empty;
+        }
+            
+        unsafe
+        {
+            fixed (char* text = new char[size])
+            {
+                PInvoke.GetWindowText(handle, text, size + 1);
+
+                return new string(text);
+            }
+        }
+    }
+
+    public static string GetClassName(HWND hWnd)
+    {
+        var size = 16;
+
+        var name = new PWSTR();
+        PInvoke.GetClassName(hWnd, name, size);
+        return name.ToString();
+    }
+}
